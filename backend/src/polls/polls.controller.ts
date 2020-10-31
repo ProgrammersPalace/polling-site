9046e5b543a8c0778fd374bb4b001000c0de01dc
HurriedHighlander <>
2020-10-31 18:28:58 -0500

    added initial poll stuff in the backend

--- backend/src/polls/polls.controller.ts
+++ backend/src/polls/polls.controller.ts
@@ -1,10 +1,17 @@
 import { Get } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
+import { get } from 'http';
+import { Observable, of } from 'rxjs';
+import { Poll } from './interfaces/poll.interface';
+import { PollsService } from './polls.service';
 
 @Controller('polls')
 export class PollsController {
+
+    constructor(private pollsService: PollsService) {}
+
     @Get()
-    getHello(): string {
-        return "This is the polls controller!";
+    getAllPolls(): Observable<Poll[]> {
+        return of(this.pollsService.findAll());
     }
 }
